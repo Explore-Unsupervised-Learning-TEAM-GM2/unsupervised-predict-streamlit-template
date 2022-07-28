a3d459be15ad63211e7d0905b4832f88205f329d
Thabang Mokoena <thabangpardonmokoena@gmail.com>
2022-07-28 05:33:44 +0200

    changed model

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -49,7 +49,7 @@ ratings_df.drop(['timestamp'], axis=1,inplace=True)
 ratings_df = ratings_df
 
 # We make use of an SVD model trained on a subset of the MovieLens 10k dataset.
-model=pickle.load(open('resources/models/SVD.pkl', 'rb'))
+model=pickle.load(open('resources/models/svd.pkl', 'rb'))
 
 def prediction_item(item_id):
     """Map a given favourite movie to users within the
